23d0bdf551873eca9896b8321988883bd10a5e00
maibarra0904 <mario.ibarra.86@gmail.com>
2023-04-20 17:03:51 -0500

    200423

--- classes/Email.php
+++ classes/Email.php
@@ -49,4 +49,36 @@ class Email {
         $mail->send();
 
     }
+
+    public function enviarInstrucciones() {
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'd5f8c9cb561b01';
+        $mail->Password = 'afd0e4571e71bd';
+
+        $mail->setFrom('admin@miempresa.com');
+        $mail->addAddress('mario.ibarra.86@gmail.com', 'MarioIbarra.com');
+        $mail->Subject = 'Restablece tu password';
+
+        //Set HTML
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p>Hola <strong> " . $this->nombre . "</strong>. Has solicitado restablecer el password de tu cuenta en App Salón.</p>";
+        $contenido .= "<p>Para restablecerlo presiona el siguiente <a href='http://localhost:3000/recuperar?token=" . $this->token ."'>
+        enlace</a></p>";
+        $contenido .= "<p>Si tu no lo solicitaste, ignora éste mensaje.</p>";
+        $contenido .= "</html>";
+
+        $mail->Body = $contenido;
+
+        //Enviar Email
+        $mail->send();
+
+    }
 }
