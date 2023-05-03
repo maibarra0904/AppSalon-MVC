b1d74bd966a8f883148912586ec53760aa34a1fd
maibarra0904 <mario.ibarra.86@gmail.com>
2023-05-03 16:03:47 -0500

    030523

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -8,6 +8,13 @@ use MVC\Router;
 class AdminController {
     public static function index(Router $router) {
         
+        $fecha = $_GET['fecha'] ?? date('Y-m-d');
+        $fechas = explode('-', $fecha);
+        //debuguear($fechas);
+        if(!checkdate($fechas[1], $fechas[2], $fechas[0])) {
+            header('Location: /404');
+        }
+
         //Consultar la base de datos
         $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
         $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
@@ -18,13 +25,14 @@ class AdminController {
         $consulta .= " ON citaServicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
         $consulta .= " ON servicios.id=citaServicios.servicioId ";
-        //$consulta .= " WHERE fecha =  '${fecha}' ";
+        $consulta .= " WHERE fecha =  '$fecha' ";
 
         $citas = AdminCita::SQL($consulta);
 
         $router->render('admin/index', [
             'nombre' => $_SESSION['nombre'],
-            'citas' => $citas
+            'citas' => $citas,
+            'fecha' => $fecha
         ]);
     }
 }
--- views/admin/index.php
+++ views/admin/index.php
@@ -10,37 +10,68 @@
     <form class="formulario">
         <div class="campo">
             <label for="fecha">Fecha</label>
-            <input type="date" id="fecha" name="fecha">
+            <input type="date" id="fecha" name="fecha" value="<?php echo $fecha; ?>">
         </div>
     </form>
 </div>
+
+<?php
+    if(count($citas) === 0){
+        echo "<h2>No hay citas en esta fecha</h2>";
+    }
+?>
 
 <div id="citas-admin">
     <ul class="citas">
         <?php
             $idCita=0;
-            foreach($citas as $cita) {
+            
+            foreach($citas as $key => $cita) {
                 
                 if($idCita !== $cita->id) {
+                    $total=0;
         ?>
-        <li>
-            <p>ID: <span><?php echo $cita->id; ?></span></p> 
-            <p>Hora: <span><?php echo $cita->hora; ?></span></p>
-            <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
-            <p>Email: <span><?php echo $cita->email; ?></span></p>
-            <p>Telefono: <span><?php echo $cita->telefono; ?></span></p>
+                    <li>
+                        <p>ID: <span><?php echo $cita->id; ?></span></p> 
+                        <p>Hora: <span><?php echo $cita->hora; ?></span></p>
+                        <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
+                        <p>Email: <span><?php echo $cita->email; ?></span></p>
+                        <p>Telefono: <span><?php echo $cita->telefono; ?></span></p>
 
-            <h3>Servicios</h3>
+                        <h3>Servicios</h3>
+                        
+                <?php 
+                            
+                } ?>
+
+                        <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio; ?></p>
+            <?php 
+                    $idCita = $cita->id;
+                    
+                    $total += $cita->precio;
             
-        <?php 
-            
-        } ?>
+                    $actual = $cita->id;
+                    $proximo = $citas[$key + 1] ->id ?? 0;
+                    
+                    if(esUltimo($actual,$proximo)) { ?>
 
-            <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio; ?></p>
+                        <p class="total">Total: <span>$ <?php echo $total; ?></span></p>
+                        
+                    <?php
+                    };
+                }; 
+            ?>
 
-        </li>
 
-        <?php $idCita = '' ?? $cita->id; }; ?>
+                    </li>
+
+        
     </ul>
     
 </div>
+
+<?php
+    $script = "
+        <script src='build/js/buscador.js'></script>
+    ";
+?>
