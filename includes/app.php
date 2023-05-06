935b966b432765961ee27142b72f437e8afa3f73
maibarra0904 <mario.ibarra.86@gmail.com>
2023-05-06 17:42:15 -0500

    060523

--- includes/app.php
+++ includes/app.php
@@ -7,3 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
+
+use Dotenv\Dotenv as Dot;
+$dotenv = Dot::createImmutable(__DIR__);
+$dotenv->safeLoad();
--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,14 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'LMip092132493@', 'appsalon_crud');
+function conectarDB() : mysqli {
+    $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
+    if(!$db) {
+        echo "Error no se pudo conectar";
+        exit;
+    }
+
+    return $db;
+}
 
 
 if (!$db) {
