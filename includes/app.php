ee4fa302b62e4c9d31a54c1ac1c9a80ae6c20294
maibarra0904 <mario.ibarra.86@gmail.com>
2023-05-06 18:18:37 -0500

    060523

--- includes/app.php
+++ includes/app.php
@@ -1,13 +1,14 @@
 <?php 
-
+use Dotenv\Dotenv as Dot;
 require 'funciones.php';
 require 'database.php';
 require __DIR__ . '/../vendor/autoload.php';
 
-// Conectarnos a la base de datos
-use Model\ActiveRecord;
-ActiveRecord::setDB($db);
-
-use Dotenv\Dotenv as Dot;
 $dotenv = Dot::createImmutable(__DIR__);
 $dotenv->safeLoad();
+    
+
+$db = conectarDB();
+use Model\ActiveRecord;
+
+ActiveRecord::setDB($db);
--- includes/database.php
+++ includes/database.php
@@ -11,9 +11,4 @@ function conectarDB() : mysqli {
 }
 
 
-if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
-    exit;
-}
+
